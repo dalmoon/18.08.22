41cf94749ecb36b044c6c4b35f1a39be9c93767a
l <l@121.143.14.109>
2018-09-11 14:01:06 +0900

    18.09.11

---
+++ ex01/src/main/java/kr/pe/patissier/service/BoardService.java
@@ -0,0 +1,17 @@
+package kr.pe.patissier.service;
+
+import java.util.List;
+
+import kr.pe.patissier.domain.BoardVO;
+
+public interface BoardService {
+	public void regist(BoardVO board) throws Exception;
+	
+	public BoardVO read(Integer bno) throws Exception;
+	
+	public void modify(BoardVO board) throws Exception;
+	
+	public void remove(Integer bno) throws Exception;
+	
+	public List<BoardVO> listAll() throws Exception;
+}
---
+++ ex01/src/test/java/kr/pe/patissier/test/BoardDAOTest.java
@@ -0,0 +1,15 @@
+package kr.pe.patissier.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class BoardDAOTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
